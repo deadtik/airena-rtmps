5d6c1890747268ae3aebd52a541508a8a0e100a8
kartikss <kartiksale.work@gmail.com>
2025-05-17 12:27:43 +0530

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   const nmsService = app.get(NmsService);
   nmsService.onModuleInit(); // This will start NMS
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
