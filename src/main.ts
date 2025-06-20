cfb1bed11d927e50cfcc6f0642aeaaed7f6620f1
kartikss <kartiksale.work@gmail.com>
2025-06-20 16:12:27 +0530

    Main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { clerkMiddleware } from '@clerk/express';
 import { NmsService } from './nms/nms.service'; // Import NmsService
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Register Clerk middleware first
-  app.use(clerkMiddleware());
+  // app.use(clerkMiddleware());
 
   // Enable CORS for frontend-backend communication
   app.enableCors({
-    origin: 'https://airena-streamui.vercel.app', // your frontend URL
+    origin: 'https://airena.app', // your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
